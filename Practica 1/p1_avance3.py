0a7e58ae8fc8be4458858e7f5c01201a8aaa2326
LeoYaMM <y.leobardomartinez@gmail.com>
2024-03-15 13:46:31 -0600

    Add binary encoding functions

---
+++ Practica 1/p1_avance3.py
@@ -0,0 +1,20 @@
+# Codificación binaria para números reales
+import numpy
+import p1_avance
+
+def seleccionDePadres():
+    #funcion
+    return
+
+def cruza():
+    #funcion
+    return
+
+def mutacion():
+    #funcion
+    return
+
+def seleccionDeSobrevivientes():
+    #funcion
+    return
+
