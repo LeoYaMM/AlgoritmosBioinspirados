23ac97c33f9b95d67f5ca97482655fdb734f18de
Diego-2019 <diego190703@hotmail.com>
2024-03-15 14:14:17 -0600

    Ruleta

--- Practica 1/p1_avance3.py
+++ Practica 1/p1_avance3.py
@@ -1,10 +1,20 @@
 # Codificación binaria para números reales
 import numpy
 import p1_avance
+import random
 
-def seleccionDePadres():
-    #funcion
-    return
+def seleccionDePadre(poblacion, fitness):
+    total_fitness = sum(fitness)
+    # Generar un número aleatorio entre 0 y la suma total de fitness
+    seleccion = random.uniform(0, total_fitness)
+    # Recorrer la población y acumular el fitness hasta que se alcance el valor aleatorio
+    acumulado = 0
+    for i, individuo in enumerate(poblacion):
+        acumulado += fitness[i]
+        if acumulado >= seleccion:
+            print(f"Valor acumulado: {acumulado}")
+            return individuo
+
 
 def cruza():
     #funcion
