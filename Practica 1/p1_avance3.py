af61f0bf207212a67da4df523bd374ceb69ac3e3
LeoYaMM <y.leobardomartinez@gmail.com>
2024-03-15 14:19:27 -0600

    Add crossover function with two parents

--- Practica 1/p1_avance3.py
+++ Practica 1/p1_avance3.py
@@ -16,8 +16,9 @@ def seleccionDePadre(poblacion, fitness):
             return individuo
 
 
-def cruza():
-    #funcion
+def cruza(padre1, padre2):
+    #funcion de cruza mediante 2 puntos
+    
     return
 
 def mutacion():
@@ -28,3 +29,7 @@ def seleccionDeSobrevivientes():
     #funcion
     return
 
+poblacion_inicial_rosenbrock = [round(random.uniform(-2.048, 2.048), 2) for _ in range(1, 11)]
+#puntos_evaluados_rosenbrock = round(rosenbrock(puntos_rosenbrock), 4)
+poblacion_inicial_ackley = [round(random.uniform(-32.768, 32.768), 2) for _ in range(1, 11)]
+#puntos_evaluados_ackley = round(ackley(puntos_ackley), 4)
