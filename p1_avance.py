4c11c7be4ef0efbc95e39057aa6b415a9babbfec
Diego-2019 <diego190703@hotmail.com>
2024-03-15 12:21:18 -0600

    Corrección funciones de Rosenbrock y Ackley en p1_avance.py

--- p1_avance.py
+++ p1_avance.py
@@ -1,37 +1,25 @@
 import numpy as np
 import random
 
-def summation(x):
-    sum = 0
-    for xi in x:
-        sum += xi
-
-    return sum
-
-
 # Define the Rosenbrock function
 def rosenbrock(x):
     resultado = 0
-    for i, punto in enumerate(x):
-        resultado  += 100 * ((punto**2) - x[i + 1])**2 + (1 - punto)**2
-        if i < len(x):
-            break
-
+    for i in range(len(x) - 1):
+        resultado += 100 * ((x[i] ** 2) - x[i + 1]) ** 2 + (1 - x[i]) ** 2
     return resultado
+
 
 # Define the Ackley function
 def ackley(x):
-    a = 20
-    b = 0.2
-    c = 2 * np.pi
-    sum_sq_term = -a * np.exp(-b * np.sqrt(sum(x**2) / len(x)))
-    cos_term = -np.exp(sum(np.cos(c * x) / len(x)))
-    return sum_sq_term + cos_term + a + np.exp(1)
+    return - 20 * np.exp(-0.2 * np.sqrt(sum(xi**2 for xi in x) / len(x))) - np.exp(sum([np.cos(2 * np.pi * xi) for xi in x]) / len(x)) + 20 + np.exp(1)
 
 
-poblacion = [a for a in range(1, 11)]
 puntos_rosenbrock = [round(random.uniform(-2.048, 2.048), 2) for _ in range(1, 11)]
-puntos_evaluados = round(rosenbrock(puntos_rosenbrock), 4)
+puntos_evaluados_rosenbrock = round(rosenbrock(puntos_rosenbrock), 4)
+puntos_ackley = [round(random.uniform(-32.768, 32.768), 2) for _ in range(1, 11)]
+puntos_evaluados_ackley = round(ackley(puntos_ackley), 4)
 
-print(f"Puntos seleccionados: {puntos_rosenbrock}")
-print(f"Resultado: {puntos_evaluados}")
+print(f"Puntos seleccionados para rosenbrock: {puntos_rosenbrock}")
+print(f"Resultado rosenbrock: {puntos_evaluados_rosenbrock}")
+print(f"Puntos seleccionados para ackley: {puntos_ackley}")
+print(f"Resultado ackley: {puntos_evaluados_ackley}")
