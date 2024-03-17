d8968f27411f7a5d1bc4817a2156fb3df575609f
Aldo Escamilla Resendiz <resendizae318@gmail.com>
2024-03-16 18:04:19 -0600

    torneo en rosenbrocks

--- Practica 1/p1_avance3.py
+++ Practica 1/p1_avance3.py
@@ -25,12 +25,38 @@ def mutacion():
     #funcion
     return
 
+def rosenbrock(x):
+    resultado = 0
+    for i in range(len(x) - 1):
+        resultado += 100 * ((x[i] ** 2) - x[i + 1]) ** 2 + (1 - x[i]) ** 2
+    return resultado
+
 def seleccionDeSobrevivientes(individuos, competencias):
+    valores_objetivo = {ind: rosenbrock for ind, x in individuos.items()}
     sobrevivientes = []
     for comp in competencias:
         ganador = comp[0] if valores_objetivo[comp[0]] > valores_objetivo[comp[1]] else comp[1]
         sobrevivientes.append(ganador)
-    return valores_objetivo, sobrevivientes
+    return sobrevivientes
+    
+def generar_individuos():
+    individuos = {}
+    for i in range(1, 11):
+        x = (random.uniform(-40, 40),2)
+        y = (random.uniform(-40, 40),2)
+        individuos[i] = (x, y)       
+    return individuos
+
+def generar_competencias(n_competencias, n_individuos):
+    competencias = []
+    for _ in range(n_competencias):
+        competidores = random.sample(range(1, n_individuos + 1), 2)  
+        competencias.append(tuple(competidores))
+    return competencias
+
+individuos_aleatorios = generar_individuos()
+competencias = generar_competencias(5, 10)
+sobrevivientes = seleccionDeSobrevivientes(individuos_aleatorios, competencias)
 
 print("\nResultados de las competiciones (sobrevivientes):")
 for i, competencia in enumerate(competencias):
